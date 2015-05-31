593ceb58ab537dcdc8ffd8bfcedc5d0f33139755
Yana <yeremenko89@gmail.com>
2015-05-31 07:55:50 +0300

    my commit

---
+++ src/main/java/pages/diaspora.java
@@ -0,0 +1,77 @@
+package pages;
+
+import com.codeborne.selenide.ElementsCollection;
+import ru.yandex.qatools.allure.annotations.Step;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Condition.text;
+/**
+ * Created by yana on 30.05.15.
+ */
+public class diaspora {
+
+    public static ElementsCollection contentsList =  $$(".markdown-content");
+    public static ElementsCollection aspectsList =  $$(".selectable");
+
+    @Step
+    public static void openMyActivity() {
+        $(byText("My activity")).click();
+    }
+
+    @Step
+    public static void openMyMentions() {
+        $(byText("@Mentions")).click();
+    }
+
+    @Step
+    public static void openFollowedtags() {
+        $(byText("#Followed tags")).click();
+    }
+
+    @Step
+    public static void openMyAspects() {
+        $(byText("My aspects")).click();
+    }
+
+    @Step
+    public static void shareNews(){
+        $("#submit").click();
+    }
+
+    @Step
+    public  static void writeText(String newText){
+        $("#status_message_fake_text").setValue(newText);
+    }
+
+    @Step
+    public  static void enterLogin(String email){
+        $("#user_username").setValue(email).pressEnter();
+    }
+
+    @Step
+    public static void enterPassword(String password){
+        $("#user_password").setValue(password).pressEnter();
+    }
+
+    @Step
+    public static void addNewAspects(String nameAspect) {
+        aspectsList.find(text("+ Add an aspect")).click();
+        $("#aspect_name").setValue(nameAspect).pressEnter();
+    }
+
+    @Step
+    public static void commentPublic(String comments){
+        $(byText("Comment")).click();
+        $("[name='text']").setValue(comments);
+        $("[name='commit']").shouldBe(visible).click();
+    }
+
+    @Step
+    public static void userPopupMenu(String nameMenuItem){
+        $(".user-menu-trigger").click();
+        $(byText(nameMenuItem)).click();
+    }
+}
---
+++ src/test/java/diasporaTests/AllDiasporaTests.java
@@ -0,0 +1,96 @@
+package diasporaTests;
+
+import com.codeborne.selenide.Screenshots;
+import com.google.common.io.Files;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ru.yandex.qatools.allure.annotations.Attachment;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static helpers.Helpers.dropCreateSeedDiasporaDB;
+import static pages.diaspora.*;
+/**
+ * Created by yana on 30.05.15.
+ */
+public class AllDiasporaTests {
+
+   /* @BeforeClass
+    public static void openDiaspora() {
+        open("http://localhost:3000/stream");
+        Configuration.timeout = 20000;
+    }*/
+
+    /*@Before
+    public void clearData() {
+        dropCreateSeedDiasporaDB();
+    }*/
+
+    @Test
+    public void diasporaTests() {
+
+        open("http://localhost:3000/stream");
+
+        // log in
+        enterLogin("bob");
+        enterPassword("evankorth");
+        $(".user-name").shouldHave(text("Bob Grimm"));
+
+        //create activity
+        openMyActivity();
+        writeText("A from activity");
+        shareNews();
+        contentsList.find(text("A from activity"));
+
+        //create mentions
+        openMyMentions();
+        writeText("D from mentios");
+        shareNews();
+        contentsList.find(text("D from mentios"));
+
+        //create mark
+        openFollowedtags();
+        writeText("T from tags");
+        shareNews();
+        contentsList.find(text("T from tags"));
+
+        //my aspects
+        openMyAspects();
+        aspectsList.shouldHave(texts("Deselect all", "generic", "+ Add an aspect"));
+
+        // add new aspect
+        addNewAspects("B");
+
+        $(byText("Stream")).click();
+        openMyAspects();
+        aspectsList.shouldHave(texts("Select all", "generic", "B", "+ Add an aspect"));
+
+        //go to prifile
+        userPopupMenu("Profile");
+        contentsList.shouldHave(texts("T from tags", "D from mentios", "A from activity"));
+
+        //comment posts
+        contentsList.find(text("T from tags"));
+        commentPublic("M");
+    }
+
+        @After
+        public void tearDown ()throws IOException {
+            screenshot();
+        }
+
+        @Attachment(type = "image/png")
+        public byte[] screenshot() throws IOException {
+            File screenshot = Screenshots.getScreenShotAsFile();
+            return Files.toByteArray(screenshot);
+        }
+}
